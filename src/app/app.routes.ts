7db6e5e8c1fc4369c955358dd12e9eaaa5067832
Arjun Naik <naikarjun10@gmail.com>
2025-05-05 22:13:31 +0530

    lazy loading in angular

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,8 +5,11 @@ import { ContactComponent } from './routing-demo/contact/contact.component';
 import { PageNotFoundComponent } from './routing-demo/page-not-found/page-not-found.component';
 
 export const routes: Routes = [
-    { path: 'contact', component: ContactComponent },
+    // { path: 'contact', component: ContactComponent },
+    { path: 'contact', loadComponent: () => import('./routing-demo/contact/contact.component').then(c => c.ContactComponent) },
     { path: '', component: HomeComponent },
+    // { path: 'home', loadComponent: () => import('./routing-demo/home/home.component').then(c => c.HomeComponent) },
     { path: 'about', component: AboutComponent },
+    // { path: 'about', loadComponent: () => import('./routing-demo/about/about.component').then(c => c.AboutComponent) },
     { path: '**', component: PageNotFoundComponent}
 ];
